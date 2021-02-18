8ee2be4a763ed79a5291a8a812866d194b62ba8a
Swapnil Bhaumik <swapnilbhaumikcfc@gmail.com>
2021-02-18 20:50:18 +0530

    Minor changes

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const app = require('express')()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
+const path = require('path');
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 io.on('connection', socket => {
-  socket.on('a', ( name, message ) => {
-    io.emit('a', { name, message })
+  socket.on('calculator', ( name, message ) => {
+    io.emit('calculator', { name, message })
   })
 })
 
 const PORT = process.env.PORT || 5000
 
 http.listen(PORT, function() {
-  console.log('listening on port 4000')
+  console.log(`listening on port ${PORT}`)
 })
 
